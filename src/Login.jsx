98c6a952c46f44b36224ebc81dc31102dc8c2457
nikhil-bisht01 <163300749+nikhil-bisht01@users.noreply.github.com>
2024-05-31 16:50:22 +0530

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,19 +1,21 @@
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom"; // Import useNavigate hook
+import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
 function Login() {
-  const [username, setUsername] = useState("");
+  const [email, setemail] = useState("");
   const [password, setPassword] = useState("");
-  const [captcha] = useState("");
   const [rememberMe, setRememberMe] = useState(false);
 
   const navigate = useNavigate(); // Initialize useNavigate hook
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
+    console.log(JSON.stringify({
+      email,
+      password,
+    }))
     try {
       const response = await fetch('https://ngd-backend.onrender.com/login', {
         method: 'POST',
@@ -21,9 +23,8 @@ function Login() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username,
+          email,
           password,
-          rememberMe,
         }),
       });
 
@@ -43,39 +44,6 @@ function Login() {
     }
   };
 
-  const [captchaText, setCaptchaText] = useState('');
-  const [userInput, setUserInput] = useState('');
-  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
-
-  const generateCaptcha = () => {
-    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#*';
-    let captcha = '';
-    for (let i = 0; i < 6; i++) {
-      captcha += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-    }
-    setCaptchaText(captcha);
-  };
-
-  const handleInputChange = (e) => {
-    setUserInput(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userInput === captchaText) {
-      alert('CAPTCHA verified. You can proceed.');
-      setIsCaptchaVerified(true);
-    } else {
-      alert('CAPTCHA verification failed. Please try again.');
-      setIsCaptchaVerified(false);
-      generateCaptcha();
-    }
-  };
-
-  useEffect(() => {
-    generateCaptcha();
-  }, []);
-
   const handleForgotPasswordClick = () => {
     navigate("/forgot-password"); // Navigate to the Forgot Password page
   };
@@ -91,11 +59,11 @@ function Login() {
               <div className="mt-8 font-medium text-black">
                 E-mail or Username*
               </div>
-              <input required type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter email or username "
+              <input required type="text" value={email} onChange={(e) => setemail(e.target.value)} placeholder="Enter email or username"
                 className="justify-center items-start px-5 py-5 mt-2.5 rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300 max-md:pr-5 h-5"
               />
               <div className="mt-5 font-medium text-black">Password*</div>
-              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password"
+              <input required type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password"
                 className="flex gap-5 px-5 py-3.5 mt-2 h-10 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300"
               />
               <div className="flex mt-5 text-neutral-700">
@@ -105,20 +73,9 @@ function Login() {
                 </div>
                 <div className="cursor-pointer" onClick={handleForgotPasswordClick}><a href="Forget">Forgot Password?</a></div> 
               </div>
-              <div className="mt-6 ml-[8%] rounded-lg border-0 border-white border-solid shadow-sm bg-gray-400 w-[80%] h-10">
-                <form onSubmit={handleSubmit}>
-                  <label htmlFor="captcha" className="text-xl ml-[35%]">{captchaText}</label>
-                </form>
-              </div>
-              <input type="captcha"
-                placeholder="Enter Captcha"
-                className="flex gap-5 px-5 py-3.5 mt-2 h-10 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300"
-                onChange={handleInputChange}
-                value={userInput}
-              />
-              <Link to="/DMS"><button type="submit" className="justify-center items-center px-16 py-5 mt-8 text-base font-semibold text-white rounded-lg border-0 border-white border-solid shadow-sm bg-violet-950 max-md:px-5 ml-[25%]">
+              <button type="submit" className="justify-center items-center px-16 py-5 mt-8 text-base font-semibold text-white rounded-lg border-0 border-white border-solid shadow-sm bg-violet-950 max-md:px-5 ml-[25%]">
                 Log in
-              </button></Link>
+              </button>
               <div className="self-center mt-14 text-base text-violet-950 max-md:mt-10">
                 <span className="">Don’t have an account?</span>
                 <span className="font-bold text-violet-950"><a href="./Signup">Sign up</a></span>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,15 +3,17 @@ import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 
 function MyComponent() {
-    const [username, setUsername] = React.useState('');
+    const [name, setname] = React.useState('');
     const [email, setEmail] = React.useState('');
-    const [phoneNumber, setPhoneNumber] = React.useState('');
+    const [phone, setPhone] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [confirmPassword, setConfirmPassword] = React.useState('');
 
-    const handleSignUp = () => {
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
         // Validate form fields
-        if (!username || !email || !phoneNumber || !password || !confirmPassword) {
+        if (!name || !email || !phone || !password || !confirmPassword) {
             alert("Please fill in all fields");
             return;
         }
@@ -21,21 +23,43 @@ function MyComponent() {
             return;
         }
 
-        // Perform sign-up logic here
-        // For demonstration purposes, let's just log the form data
-        console.log({
-            username: username,
-            email: email,
-            phoneNumber: phoneNumber,
-            password: password
-        });
+        try {
+            const response = await fetch('https://ngd-backend.onrender.com/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    phone,
+                    password,
+                }),
+            });
 
-        // Clear form fields after successful sign up
-        setUsername('');
-        setEmail('');
-        setPhoneNumber('');
-        setPassword('');
-        setConfirmPassword('');
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Sign-up successful:', data);
+                alert('Sign-up successful!');
+                // Clear form fields after successful sign up
+                setUsername('');
+                setEmail('');
+                setPhoneNumber('');
+                setPassword('');
+                setConfirmPassword('');
+            } else {
+                if(response.status===409){ 
+                    alert("User Already Exist")
+                    return
+                }
+                const errorData = await response.json();
+                console.error('Sign-up failed:', errorData);
+                alert('Sign-up failed. Please try again.');
+            }
+        } catch (error) {
+            console.error('Error during sign-up:', error);
+            alert('An error occurred. Please try again later.');
+        }
     };
 
     return (
@@ -45,18 +69,19 @@ function MyComponent() {
             <div className="flex flex-col rounded-2xl  bg-neutral-100 mb-[-10%]">
                 <div className="flex flex-col self-center px-10 pt-6 pb-11 mt-14  text-sm rounded-2xl border border-solid shadow-sm bg-neutral-100 border-stone-300 text-zinc-500 w-[403px] max-md:px-5 max-md:mt-10">
                     <div className="mt-9 ml-[35%] font-bold text-2xl text-black">SIGN UP</div>
-                    <div className="mt-9 font-medium text-black">Username*</div>
-                    <input type="text" className="justify-center items-start px-5 py-5 h-10 mt-2.5 rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300 max-md:pr-5" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <div className="mt-5 font-medium text-black">Email* </div>
-                    <input type="email" className="flex gap-3 px-4 py-3.5 mt-2 rounded-lg h-10 border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <div className="mt-5 font-medium text-black">Phone number* </div>
-                    <input type="tel" className="justify-center items-start px-12 py-5 mt-2 h-10 rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300 max-md:px-5" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                    <div className="mt-4 font-medium text-black">Password*</div>
-                    <input type="password" className="flex gap-5 justify-between px-5 py-3.5 h-10 mt-2 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <div className="mt-4 font-medium text-black">Confirm Password*</div>
-                    <input type="password" className="flex gap-5 justify-between px-5 py-3.5 h-10 mt-2 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                    <button type="submit" className="justify-center items-center px-16 py-5 mt-8 text-sm font-semibold text-white rounded-lg border-0 border-white border-solid shadow-sm bg-violet-950 max-md:px-5">Sign up
-                    </button>
+                    <form onSubmit={handleSignUp}>
+                        <div className="mt-9 font-medium text-black">Username*</div>
+                        <input type="text" className="justify-center items-start px-5 py-5 h-10 mt-2.5 rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300 max-md:pr-5" value={name} onChange={(e) => setname(e.target.value)} />
+                        <div className="mt-5 font-medium text-black">Email* </div>
+                        <input type="email" className="flex gap-3 px-4 py-3.5 mt-2 rounded-lg h-10 border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <div className="mt-5 font-medium text-black">Phone number* </div>
+                        <input type="tel" className="justify-center items-start px-12 py-5 mt-2 h-10 rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300 max-md:px-5" placeholder="0987654321" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                        <div className="mt-4 font-medium text-black">Password*</div>
+                        <input type="password" className="flex gap-5 justify-between px-5 py-3.5 h-10 mt-2 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <div className="mt-4 font-medium text-black">Confirm Password*</div>
+                        <input type="password" className="flex gap-5 justify-between px-5 py-3.5 h-10 mt-2 w-full whitespace-nowrap rounded-lg border border-solid bg-neutral-200 bg-opacity-0 border-stone-300" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                        <button type="submit" className="justify-center items-center px-16 py-5 mt-8 text-sm font-semibold text-white rounded-lg border-0 border-white border-solid shadow-sm bg-violet-950 max-md:px-5">Sign up</button>
+                    </form>
                     <div className="flex justify-center mt-3.5 text-base text-violet-950">
                         Already have an account? <span className="font-bold cursor-pointer"><a href="./Login">Login</a></span>
                     </div>
@@ -66,5 +91,4 @@ function MyComponent() {
         </div>
     );
 }
-
 export default MyComponent;
